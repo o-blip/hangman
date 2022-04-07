0fabb1d45d0c17a66fb2d552cb02506bff0b7d11
piero <dolibiroli@gmail.com>
2022-04-07 13:37:02 -0500

    Initial commit

--- lib/game_logic.rb
+++ lib/game_logic.rb
@@ -76,25 +76,6 @@ class Game
   end 
 end
 
-class Player
-  def guess_letter(word)
-    letter = gets.chomp.downcase
-    if letter == word
-      return letter
-    end
-    if letter.length > 1
-      puts 'Invalid guess, guess 1 letter'
-      self.guess_letter
-    elsif !letter?(letter) 
-      puts 'Invalid guess, guess a letter'
-      self.guess_letter
-    end
-    letter
-  end
 
-  def letter?(char)
-    char =~ /[A-Za-z]/
-  end
-end
 
 game = Game.new.play
